4977ea1410e9e4201aee3a8ff9df746f8af2e908
Dong Hyeon Kim <donghyeon@happymoonday.com>
2023-10-08 10:46:06 +0900

    feat: add LadderView

---
+++ app/src/main/java/com/example/ghostleg/view/custom/LadderView.kt
@@ -0,0 +1,12 @@
+package com.example.ghostleg.view.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class LadderView(
+    context: Context,
+    attr: AttributeSet?
+): View(context, attr) {
+
+}
