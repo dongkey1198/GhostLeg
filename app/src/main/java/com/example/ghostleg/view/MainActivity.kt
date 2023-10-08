6a70fe066fc76e2aa6f981fcfc208ba89c8955df
Dong Hyeon Kim <donghyeon@happymoonday.com>
2023-10-08 12:33:00 +0900

    feat: add function to draw vertical lines

--- app/src/main/java/com/example/ghostleg/view/MainActivity.kt
+++ app/src/main/java/com/example/ghostleg/view/MainActivity.kt
@@ -2,25 +2,47 @@ package com.example.ghostleg.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.ViewTreeObserver
+import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.ghostleg.databinding.ActivityMainBinding
+import com.example.ghostleg.viewmodel.MainViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initLadderView()
         observeLadderView()
+    }
+
+    private fun initLadderView() {
+        lifecycleScope.launch(Dispatchers.Main) {
+            viewModel.verticalLinesFlow.collect {
+                binding.ladderView.updateVerticalLines(it)
+            }
+        }
     }
 
     private fun observeLadderView() {
         with(binding.ladderView) {
-            viewTreeObserver.addOnGlobalLayoutListener {
-                Log.d("aaa", "$height, $width")
-            }
+            viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+                override fun onGlobalLayout() {
+                    viewModel.generateVerticalLines(
+                        playerNumber = 6,
+                        width = width.toFloat(),
+                        height = height.toFloat()
+                    )
+                    viewTreeObserver.removeOnGlobalLayoutListener(this)
+                }
+            })
         }
     }
 }
--- app/src/main/java/com/example/ghostleg/view/custom/LadderView.kt
+++ app/src/main/java/com/example/ghostleg/view/custom/LadderView.kt
@@ -1,12 +1,40 @@
 package com.example.ghostleg.view.custom
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import com.example.ghostleg.model.Line
 
-class LadderView(
-    context: Context,
-    attr: AttributeSet?
-): View(context, attr) {
+class LadderView(context: Context, attr: AttributeSet?): View(context, attr) {
 
+    private val _verticalLines: MutableList<Line> = mutableListOf()
+    private val paint = Paint()
+
+    fun updateVerticalLines(lines: List<Line>) {
+        _verticalLines.apply {
+            clear()
+            addAll(lines)
+        }
+        invalidate()
+    }
+
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        canvas?.let {
+            drawVerticalLines(it)
+        }
+    }
+
+    private fun drawVerticalLines(canvas: Canvas) {
+        _verticalLines.forEach { ladder ->
+            paint.apply {
+                color = ladder.color
+                strokeWidth = ladder.stroke
+            }
+            canvas.drawLine(ladder.startX, ladder.startY, ladder.endX, ladder.endY, paint)
+        }
+    }
 }
