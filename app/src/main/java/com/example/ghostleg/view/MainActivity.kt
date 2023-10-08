d62b8d4e6db145871d25f4b35612fc2059122b82
Dong Hyeon Kim <donghyeon@happymoonday.com>
2023-10-08 10:48:52 +0900

    feat: add viewTreeObserver to measure ladderView's width and height

--- app/src/main/java/com/example/ghostleg/view/MainActivity.kt
+++ app/src/main/java/com/example/ghostleg/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.ghostleg
+package com.example.ghostleg.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.ghostleg.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        observeLadderView()
+    }
+
+    private fun observeLadderView() {
+        with(binding.ladderView) {
+            viewTreeObserver.addOnGlobalLayoutListener {
+                Log.d("aaa", "$height, $width")
+            }
+        }
     }
 }
