18c82987866d88971b0992303b045edcbef397c0
Dong Hyeon Kim <donghyeon@happymoonday.com>
2023-10-11 13:40:08 +0900

    feat: add SettingActivity

---
+++ app/src/main/java/com/example/ghostleg/view/setting/SettingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ghostleg.view.setting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ghostleg.R
+
+class SettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+}
