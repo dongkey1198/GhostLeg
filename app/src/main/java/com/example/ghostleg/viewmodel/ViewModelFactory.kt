3867cfc885501d02ff3f86aabc451873a89119cb
Dong Hyeon Kim <donghyeon@happymoonday.com>
2023-10-11 16:52:30 +0900

    feat: implement ViewModelFactory

---
+++ app/src/main/java/com/example/ghostleg/viewmodel/ViewModelFactory.kt
@@ -0,0 +1,29 @@
+package com.example.ghostleg.viewmodel
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.ghostleg.data.datastore.DataStoreManager
+import com.example.ghostleg.data.datastore.LadderGameDataStore
+import com.example.ghostleg.data.repository.LadderGameRepositoryImpl
+import com.example.ghostleg.viewmodel.setting.SettingViewModel
+
+class ViewModelFactory(
+    private val context: Context
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return when {
+            modelClass.isAssignableFrom(SettingViewModel::class.java) -> {
+                SettingViewModel(
+                    LadderGameRepositoryImpl(
+                        LadderGameDataStore(DataStoreManager(context).getLadderGameDataStore())
+                    )
+                ) as T
+            }
+            else -> {
+                throw IllegalArgumentException("Unknown ViewModel Class")
+            }
+        }
+    }
+}
